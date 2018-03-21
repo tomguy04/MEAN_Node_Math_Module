9057ddc2b2582af8259a8e5ceecf03246aea6e0a
Glen Goulter <glenrg@gmail.com>
2018-03-20 20:37:51 -0700

    final version v2 using objects from the module

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var my_module = require('./mathlib')();     //notice the extra invocation parentheses!
-//console.log(my_module);
-my_module.add(1,2); 
-my_module.multiply(2,3);
-my_module.square(3);
-my_module.random(10,20);
+const my_module = require('./mathlib');     
+const math = new my_module();
+console.log(math.add(1,2)); 
+console.log(math.multiply(2,3));
+console.log(math.square(3));
+console.log(math.random(10,20));
 
 // my_module.add(5,6); 
 
--- mathlib.js
+++ mathlib.js
@@ -11,29 +11,23 @@ module.exports = function (){
     return {
         add: function(num1, num2) { 
             // add code here 
-            console.log(num1 + num2);
+            return(num1 + num2);
         },
         multiply: function(num1, num2) {
             // add code here 
-            console.log( num1 * num2) ;
+            return( num1 * num2) ;
         },
         square: function(num) {
             // add code here 
-            console.log( num * num);
+            return( num * num);
         },
         random: function(num1, num2) {
-            // add code here
-            // needs to return a random number between the two numbers 
-            //(e.g. math.random(1,35) should return a random number between 1 and 35)
-            
-            //console.log(Math.floor(Math.random() * 10) + 1);  // returns a number between 1 and 10
-            //console.log(Math.floor(Math.random() * num2) + num1);  // returns a number between num1 and num2
             let min = Math.ceil(num1);
             let max = Math.floor(num2);
-            console.log(Math.floor(Math.random() * (max - min)) + min); //The maximum is exclusive and the minimum is inclusive
+            return(Math.floor(Math.random() * (max - min)) + min); //The maximum is exclusive and the minimum is inclusive
             //Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 
         }
-    }
     };
+};
           
