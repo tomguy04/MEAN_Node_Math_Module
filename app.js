bb1e397f771d9b95bf8254542f8105b28908f6fc
Glen Goulter <glenrg@gmail.com>
2018-03-20 20:20:04 -0700

    final version

---
+++ app.js
@@ -0,0 +1,9 @@
+var my_module = require('./mathlib')();     //notice the extra invocation parentheses!
+//console.log(my_module);
+my_module.add(1,2); 
+my_module.multiply(2,3);
+my_module.square(3);
+my_module.random(10,20);
+
+// my_module.add(5,6); 
+
